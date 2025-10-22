f11fb57e6c7604434be97bd6ce36ceb4d1e5164e
YodeSkye <YodeSkye@outlook.com>
2025-10-22 14:40:51 -0400

    VLC Upgrade

--- FullScreen.vb
+++ FullScreen.vb
@@ -1,7 +1,10 @@
-﻿Imports Skye
+﻿
+Imports Skye
 
 Public Class FullScreen
+
     Private Sub FullScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         WinAPI.HideFormInTaskSwitcher(Handle)
     End Sub
+
 End Class
