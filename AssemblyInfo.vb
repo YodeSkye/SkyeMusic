69346280648261f8ba37a3810209af0b18039ed6
YodeSkye <YodeSkye@outlook.com>
2026-05-06 06:55:43 -0400

    Skye Music v1.5: system volume, Companion, changelog
    Updated version to 1.5. Added full system-level volume control and improved Companion App support (playlist, streaming, device handling). Updated ChangeLog and Help with new features, shortcuts, and UI details. Improved album art, logging, and fixed multiple bugs. Refined resource strings and installer version.

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -1,5 +1,5 @@
 ﻿
-<Assembly: Reflection.AssemblyVersionAttribute("1.4.0")>
+<Assembly: Reflection.AssemblyVersionAttribute("1.5.0")>
 <Assembly: Reflection.AssemblyProductAttribute("SkyeMusic")>
 <Assembly: Reflection.AssemblyTitleAttribute("Skye Music")>
 <Assembly: Reflection.AssemblyDescriptionAttribute("Skye's Music Library And Player")>
